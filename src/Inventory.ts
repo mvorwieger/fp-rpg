dd61b2651bbb9c1438b789aab52e7215ddc04036
mvorwieger <michel.vorwieger@mediaman.de>
2018-08-08 11:52:49 +0200

    fixed bug in filter function that was using === instead of !==

--- src/Inventory.ts
+++ src/Inventory.ts
@@ -4,13 +4,17 @@ export interface Inventory {
     items: Item[]
 }
 
+export const createInventoryAdder = (inventory: Inventory) => (item: Item) => addItemToInventory(inventory, item)
+
+export const createInventoryRemover = (inventory: Inventory) => (name: string) => removeItemFromInventoryByName(inventory, name)
+
 export const initInventory = (items?: Item[]): Inventory => ({items: items ? items : []})
 
 export const addItemToInventory = (inventory: Inventory, item: Item): Inventory => ({...inventory, items: [...inventory.items, item]})
 
 export const removeItemFromInventoryByName = (inventory: Inventory, name: string): Inventory => ({
         ...inventory,
-        items: [...inventory.items.filter((item: Item) => item.name === name)]
+        items: [...inventory.items.filter((item: Item) => item.name !== name)]
     })
 
 export const resetInventory = (inventory: Inventory): Inventory => ({
